c87c0c231176d027a9676bc8ed55589875cf2324
gsphanikumar <gsphanikumar@gmail.com>
2012-10-20 19:35:37 +0530

    Initial commit

---
+++ src/mytest/server.clj
@@ -0,0 +1,11 @@
+(ns mytest.server
+  (:require [noir.server :as server]))
+
+(server/load-views "src/mytest/views/")
+
+(defn -main [& m]
+  (let [mode (keyword (or (first m) :dev))
+        port (Integer. (get (System/getenv) "PORT" "8080"))]
+    (server/start port {:mode mode
+                        :ns 'mytest})))
+
---
+++ src/mytest/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns mytest.views.welcome
+  (:require [mytest.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to mytest"]))
