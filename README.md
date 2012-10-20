94ff2f1466f61672b7e8c43a076d818c1e904f7b
gsphanikumar <gsphanikumar@gmail.com>
2012-10-20 20:10:50 +0530

    Fixing incorrect merge

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
+
 my-cljapp
 =========
 
 My first Clojure web app
-=======
-# my-website
-
-A website written in noir. 
-
 ## Usage
 
 ```bash
@@ -20,5 +15,3 @@ lein run
 Copyright (C) 2011 FIXME
 
 Distributed under the Eclipse Public License, the same as Clojure.
-
->>>>>>> e176066b65db091aae20e08a0e98ffafb00c28a7
--- project.clj
+++ project.clj
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-(defproject mytest "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
-            :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.1"]]
-            :main mytest.server)
-=======
 (defproject my-website "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]]
             :main my-website.server)
->>>>>>> e176066b65db091aae20e08a0e98ffafb00c28a7
-
